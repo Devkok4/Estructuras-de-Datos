1685f119fce9e26e489f1f72771673b197dc4ba1
Devkok4 <oskarcermeno1@gmail.com>
2025-02-26 23:11:10 -0600

    Clase Pila agregada

--- src/EstructurasDeDatos/DinamicStack.java
+++ src/EstructurasDeDatos/DinamicStack.java
@@ -1,5 +1,50 @@
 package EstructurasDeDatos;
 
 public class DinamicStack {
+	private Nodo head;
+	public DinamicStack() {
+		this.head=null;
+	}
+	
+	public void insertarDatoStack(int dato) {
+		Nodo nuevoNodo= new Nodo(dato);
+		
+		if(head==null) {
+			head=nuevoNodo;
+			head.setSiguiente(null);
+		}
+		
+		else {
+			nuevoNodo.setSiguiente(head);
+			head=nuevoNodo;
+		}
+	}
+	
+	public void obtenerDatoStack() {
+		if(head==null) {
+			System.out.println("No hay datos.");
+			
+		}
+		
+		else {
+			System.out.println("Dato Extraido -> "+ head.getDato());
+			head= head.getSiguiente();
+		}
+	}
+	public void mostrarDatosStack() {
+		if(head==null) {
+			System.out.println("No hay datos.");
+			
+		}
+		
+		else {
+			Nodo temp=head;
+			while(temp!=null) {
+				System.out.println("Dato Extraido -> "+ temp.getDato());
+				temp= temp.getSiguiente();
+			}
 
+		}
+	}
+	
 }
